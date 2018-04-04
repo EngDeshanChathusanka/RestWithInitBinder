e06373446572459fce60884398abf976840cd137
Deshan <deshan@atlassian.com>
2018-04-04 22:02:07 +0530

    add static call inside controller

--- src/main/java/com/dataBinder/DetailController.java
+++ src/main/java/com/dataBinder/DetailController.java
@@ -1,12 +1,22 @@
 package com.dataBinder;
 
 import com.JsonData.Request;
-import com.data.*;
+import com.data.Bank;
+import com.data.Company;
+import com.data.CompanyPropertyEditor;
+import com.data.Person;
+import com.data.PersonPropertyEditor;
+import com.data.PropertyEditorFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by deshan on 3/9/2018.
@@ -22,19 +32,17 @@ public class DetailController
     public void initBinderAll(WebDataBinder binder) {
         binder.registerCustomEditor(Person.class,  propertyEditorFactory.getPropertyEditor( Person.class ));
         binder.registerCustomEditor(Company.class,  propertyEditorFactory.getPropertyEditor( Company.class ));
-        /*binder.registerCustomEditor(Person.class,  new PersonPropertyEditor());
-        binder.registerCustomEditor(Company.class,  new CompanyPropertyEditor());*/
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "api/test")
-    public ResponseEntity<String> show( @RequestParam(value = "Person", required = false) Person person,
-                                        @RequestParam(value = "Company", required = false) Company company)
+    public ResponseEntity<String> personGET(@RequestParam(value = "Person", required = false) Person person,
+                                            @RequestParam(value = "Company", required = false) Company company)
     {
         return new ResponseEntity<String>( person.getName()+"-"+company.getName() , HttpStatus.OK );
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "api/test")
     public ResponseEntity<String> personPOST(@RequestBody Request requestBody) {
-        return new ResponseEntity<String>(requestBody.getBank() + "-" + requestBody.getBranch(), HttpStatus.OK);
+        return new ResponseEntity<String>(requestBody.getBank() + "-" + requestBody.getBranch() + Bank.getInstance().getGreeting(), HttpStatus.OK);
     }
 }
